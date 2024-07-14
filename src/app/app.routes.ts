ffeec38ceb836711378f0de01c742979588ea13a
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-14 19:38:27 +0100

    feat: defined intial routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FirstpageComponent } from './firstpage/firstpage.component';
+import { SecondpageComponent } from './secondpage/secondpage.component';
+import { NativepageComponent } from './nativepage/nativepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "firstpage",
+        component: FirstpageComponent
+    },
+    {
+        path: "secondpage",
+        component: SecondpageComponent
+    },
+    {
+        path: "",
+        component: NativepageComponent
+    }
+];
