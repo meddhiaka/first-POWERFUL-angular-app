2e2eb9277c774dee377867cf4930864374c1a379
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-14 19:39:29 +0100

    feat: first page used for routing with lazy loading of components

---
+++ src/app/firstpage/firstpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-firstpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './firstpage.component.html',
+  styleUrl: './firstpage.component.css'
+})
+export class FirstpageComponent {
+
+}
