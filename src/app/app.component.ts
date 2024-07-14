2914270e0384870e8a226bde35ac0e9dce33dd21
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-14 19:38:05 +0100

    feat: added Router, NgOptimizedImage, Outlet modules ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { StatexComponent } from './statex/statex.component';
 import { CondtionalcomponentrenderComponent } from './condtionalcomponentrender/condtionalcomponentrender.component';
 import { ForlooprenderingComponent } from './forlooprendering/forlooprendering.component';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StatexComponent, CondtionalcomponentrenderComponent, ForlooprenderingComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    StatexComponent,
+    CondtionalcomponentrenderComponent,
+    ForlooprenderingComponent,
+    RouterLink,
+    NgOptimizedImage
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
