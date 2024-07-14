aa3e948d78588f9c2641e7393b35da1105a63060
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-07-14 19:39:46 +0100

    feat: native/ Home page used for routing with lazy loading of components

---
+++ src/app/nativepage/nativepage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nativepage',
+  standalone: true,
+  imports: [],
+  templateUrl: './nativepage.component.html',
+  styleUrl: './nativepage.component.css'
+})
+export class NativepageComponent {
+
+}
